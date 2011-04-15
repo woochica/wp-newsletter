0c2446b0c8829f9feae00a28d2083097384412bc
Török Gábor <gabor@20y.hu>
2011-04-15 11:52:52 +0200

    Apply the standard `widget_title` filter on title.

--- widget.php
+++ widget.php
@@ -14,7 +14,7 @@ class Newsletter_Widget extends WP_Widget {
                 extract($args);
 
                 // Each widget can store its own options. We keep strings here.
-                $title = $instance['title'];
+                $title = apply_filters('widget_title', $instance['title']);
                 $text = $instance['text'];
                 $form = $instance['form'];
 
